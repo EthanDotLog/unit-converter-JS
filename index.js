3cfb86a6bb5c484e193880d43603f09caf7823be
EthanDotLog <148747739+EthanDotLog@users.noreply.github.com>
2023-10-25 18:06:47 -0500

    added - basic css html finished javascript now works, gets input and controls it all via button press.

--- index.js
+++ index.js
@@ -1,42 +1,53 @@
+//elements from the HTML
 const userInputEl = document.querySelector("#user-input");
 const resultsLengthEl = document.querySelector("#results-length-el");
-const compLengthEl = document.querySelector("#comp-length-el");
 const resultsVolumeEl = document.querySelector("#results-volume-el");
-const compVolumeEl = document.querySelector("#comp-volume-el");
 const resultsMassEl = document.querySelector("#results-mass-el");
-const compMassEl = document.querySelector("#comp-mass-el");
+//the main input array - it is by default a number
+const userNumArr = [];
+//user string and number from array
+let userStr;
+let userNum;
+const conversion = {length: 0.3048, volume: 3.7854, mass: 0.453592}
 
 
-
-
-const userNum = [];
-
-
-
-
-
-userInputEl.addEventListener("keyup", function(event) {
+//this grabs every keypress in the input field, calls userInput after every check
+//it logs out the user data only allowing numbers as well as pushes the numbers in an array
+userInputEl.addEventListener("keydown", function(event) {
 let num = Number(event.key);
 if (event.key === "Backspace") {
-    userNum.pop();
-    write()
-} else if (num >= 0) {
-    userNum.push(num);
-    write()
+    userNumArr.pop();
+    userInput()
+} else if (num >= 0 && num <= 9) {
+    userNumArr.push(num);
+    userInput()
 } else {
-    console.log("you should not see this");
-}   write()
+    event.preventDefault();
+    userInput()
+} 
 });
 
+//calls out the value of the array and sets userStr and Num respectively
+function userInput() {
+    let raw = "";
+    for (let i = 0; i < userNumArr.length; i++) {
+    raw += userNumArr[i];
+    }
+    userStr = raw.trim()
+    userInputEl.textContent = userStr;
+    userNum = Number(userStr);
+    resultsLengthEl.textContent = `${userStr} Meters = ${divide(conversion.length)} Feet | ${userStr} Feet = ${multi(conversion.length)} Meters`;
+    resultsVolumeEl.textContent = `${userStr} Liters = ${divide(conversion.volume)} Gallons | ${userStr} Gallons = ${multi(conversion.volume)} Liters`;
+    resultsMassEl.textContent = `${userStr} Kilograms = ${divide(conversion.mass)} Pounds | ${userStr} Pounds = ${multi(conversion.mass)} Kilograms`
+}
 
 
-
-
-
-function write() {
-    let str;
-    for (i = 0; i < userNum.length; i ++)
-    str += userNum[i];
-    userInputEl.textContent = str;
+function divide(x) {
+    let sum = userNum / x;
+    return sum
+}
+function multi(x) {
+    let sum = userNum * x;
+    return sum
 }
 
